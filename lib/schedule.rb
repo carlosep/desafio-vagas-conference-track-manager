cb565083625d495a4e1336dc7157102045d8ea25
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-03-28 09:20:21 -0300

    Broke one huge method into several smaller methods

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -6,36 +6,43 @@ class Schedule
     aux = []
     track = 0
     loop do
-      #One Method
-      morning = 180
-      afternoon = 240
-      morning_schedule = []
-      afternoon_schedule = []
       track += 1
-      array.each do |a|
-        time = a[1].to_i
-        if(morning - time >= 0)
-          morning -= time
-          morning_schedule << a[0]
-          aux << a
-        elsif(afternoon - time >= 0)
-          afternoon -= time
-          afternoon_schedule << a[0]
-          aux << a
-        end
-      end
-
-      #Another Method
-      puts "track #{track}"
-      puts '----------------'
-      puts "Morning"
-      puts morning_schedule.inspect
-      puts '----------------'
-      puts "Afternoon"
-      puts afternoon_schedule.inspect
-      puts '################'
+      set_schedule(array, aux, track)
       array -= aux
       break if ((array).length == 0)
     end
+  end
+
+  private
+
+  def set_schedule(array, aux, track)
+    morning = 180
+    afternoon = 240
+    morning_schedule = []
+    afternoon_schedule = []
+    array.each do |a|
+      time = a[1].to_i
+      if(morning - time >= 0)
+        morning -= time
+        morning_schedule << a[0]
+        aux << a
+      elsif(afternoon - time >= 0)
+        afternoon -= time
+        afternoon_schedule << a[0]
+        aux << a
+      end
+    end
+    display_schedule(track, morning_schedule, afternoon_schedule)
+  end
+
+  def display_schedule(track, m, a)
+    puts "track #{track}"
+    puts '----------------'
+    puts "Morning"
+    puts m.inspect
+    puts '----------------'
+    puts "Afternoon"
+    puts a.inspect
+    puts '################'
   end
 end
